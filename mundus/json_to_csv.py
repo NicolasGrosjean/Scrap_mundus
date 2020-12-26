26b922c991ff5ddc5e77a7b43caac97a0d23c605
NicolasGrosjean <grosjen@hotmail.fr>
2020-12-26 17:11:08 +0100

    Ajout de statistiques

--- mundus/json_to_csv.py
+++ mundus/json_to_csv.py
@@ -6,13 +6,14 @@ import argparse
 GAMES = ['Ancient Cities', 'Ancient Worlds : Egypt', 'Builders of China', 'Builders of Egypt', 'Crusader Kings 3',
          'Crusader Kings III', 'Empire : Total War', 'Europa Universalis IV', 'Fields of History: The Great War',
          'Hearts of Iron', 'Hegemony III', 'Hellish quart', 'Humankind', 'Imperator : Rome', 'Knights of Honor 2',
-         'La Porte des Fauconniers', 'Medieval II - Total War', 'Mount & Blade II', 'Nebuchadnezzar',
-         'Pharaoh : A New Era', 'Rome : Total War', 'Stadium Renovator', 'Stellaris', 'Sumerians',
+         'La Porte des Fauconniers', 'Medieval II - Total War', 'Medieval II : Total War', 'Mount & Blade II',
+         'Nebuchadnezzar', 'Pharaoh : A New Era', 'Rome : Total War', 'Stadium Renovator', 'Stellaris', 'Sumerians',
          'Supermarket Manager', 'Three Kingdoms', 'Total War : Warhammer', 'Troy : Total War']
 
 GAME_MAPPING = {'Crusader Kings III': 'Crusader Kings 3', 'Hearts of Iron': 'Hearts of Iron IV',
                 'Mount & Blade II': 'Mount & Blade II : Bannerlord', 'Three Kingdoms': 'Total War : Three Kingdoms',
-                'Total War : Warhammer': 'Total War : Warhammer II'}
+                'Total War : Warhammer': 'Total War : Warhammer II',
+                'Medieval II - Total War': 'Medieval II : Total War'}
 
 def get_args():
     parser = argparse.ArgumentParser(description='Convert JSON result to CSV and try inferring games')
