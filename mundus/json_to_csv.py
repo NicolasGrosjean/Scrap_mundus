80ad3f3a48ea583a11b4eef49bb32f1e94018eab
Nicolas Grosjean <nicolas.grosjean@neovision.fr>
2021-12-31 19:31:10 +0100

    Add 2021 data and new stats
    - 2021 data have been extracted this afternoon
    - New stats are computed to compare years

--- mundus/json_to_csv.py
+++ mundus/json_to_csv.py
@@ -6,14 +6,14 @@ import argparse
 GAMES = ['Age of Empires III : Definitive Edition', 'Age of Empires II', 'Age of Empires IV', 'Ancient Cities', 'Ancient Worlds : Egypt',
          'Builder Simulator', 'Builders of China', 'Builders of Egypt',
          'Chernobyl Liquidators Simulator', 'Chinese City Constructor ', 'Crusader Kings 3',
-         'Crusader Kings III', 'Decisive Campaigns : Ardennes Offensive', 'Distant Kingdoms',
+         'Crusader Kings III', 'Decisive Campaigns : Ardennes Offensive', 'Diarium Strategorum', 'Distant Kingdoms',
          'El Dorado: The Golden City Builder', 'Empire : Total War', 'Europa Universalis IV', 'Espresso Tycoon',
          'Field of Glory II : Medieval', 'Fields of History: The Great War', 'Going Medieval',
          'Hearts of Iron', 'HOI4', 'Hegemony III', 'Hellish Quart', 'Hellish quart', 'Humankind', 'Imperator : Rome', 'Knights of Honor 2',
          'La Porte des Fauconniers', 'Medieval II - Total War', 'Medieval II : Total War', 'Monuments Flipper', 'Mount & Blade II',
          'Nebuchadnezzar', 'Patron', 'Pharaoh : A New Era', 'Pizza Simulator', 'Project Wunderwaffe', 'Rome : Total War', 'Stadium Renovator', 'Stellaris', 'Sumerians',
          'Supermarket Manager', 'The Troop', 'Three Kingdoms', 'Timberborn', 'Total War : Troy', 'Total War : Warhammer',
-         'Total War : WARHAMMER', 'Troy : Total War', 'Victoria 3', 'Victoria III ', 'Viking City Builder', 'Wargame Red Dragon']
+         'Total War : WARHAMMER', 'Troy : Total War', 'Victoria 3', 'Victoria III', 'Viking City Builder', 'Wargame Red Dragon']
 
 GAME_MAPPING = {'Crusader Kings III': 'Crusader Kings 3', 'Hearts of Iron': 'Hearts of Iron IV', 'Hellish Quart': 'Hellish quart', 'HOI4': 'Hearts of Iron IV',
                 'Mount & Blade II': 'Mount & Blade II : Bannerlord', 'Three Kingdoms': 'Total War : Three Kingdoms',
