25a6b64e79a2f8ed7cc55ed387b78f37313e4d74
Nicolas Grosjean <nicolas.grosjean@neovision.fr>
2022-01-01 13:59:13 +0100

    Add script for extraction posts
    It would be useful for Diarium Strategorum.

--- README.md
+++ README.md
@@ -46,6 +46,8 @@ scrapy crawl mundus_article -o data/mundus_article.json
 Nota: You maybe need to update MAX_PAGE constant in 
 `mundus/mundus/spiders/article.py` to change the number of scraped pages.
 
+We need to replace manually the words *Aujourd'hui* and *Hier* by valid dates.
+
 ## Convert the data in CSV
 
 Run the following script to convert the data into a CSV file.
@@ -53,6 +55,16 @@ Run the following script to convert the data into a CSV file.
 ```
 python json_to_csv.py mundus_article.json
 ```
+
+## Extract data of a month
+
+Run the following script to extract posts of a month (December 2021 in example) to help to write the Diarium Strategorum.
+
+```
+python extract_month_posts.py mundus_article.csv 2021-12
+```
+
+The generated BBcode is written in the file *Diarium_Strategorum_2021-12.txt* (2021-12 is the input in the command).
 
 ## Run notebook to compute graphs
 
---
+++ mundus/extract_month_posts.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+import pandas as pd
+
+GAME_TYPES = ['Total War', 'Grande Stratégie', 'Gestion', 'STR']
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Extract posts of a month to help to write the Diarium Strategorum.')
+    parser.add_argument('csv', type=str, help='Relative data path (.csv)')
+    parser.add_argument('month', type=str, help='Month of the extraction in YYYY-MM format')
+    return parser.parse_args()
+
+def get_data_dir():
+    if 'data' in os.listdir('.'):
+        return 'data'
+    return os.path.join('..', 'data')
+
+def game_type_to_bbcode(game_type: str, data: pd.DataFrame):
+    """
+    Export in a string the BBcode of the dataframe with games sorted alphabetically
+    """
+    res = f'[b]{game_type}[/b]\n'
+    for game in sorted(data['game'].unique()):
+        res += game_post_to_bbcode(game, data[data['game'] == game])
+    return res
+
+def game_post_to_bbcode(game: str, posts: pd.DataFrame):
+    """
+    Export in a string the BBcode of the dataframe of the posts of a game
+    """
+    res = f'[b]{game}[/b]\n'
+    res += '[list]\n'
+    for i in reversed(range(len(posts))):
+        post = posts.iloc[i]
+        res += f'[*] [url={post["url"]}]{post["title"]}[/url]\n'
+    res += '[/list]\n\n'
+    return res
+
+
+def main(args):
+    relative_csv_path = args.csv
+    csv_path = os.path.join(get_data_dir(), relative_csv_path)
+    print(f'Read the file {csv_path}')
+    data = pd.read_csv(csv_path, sep=';')
+    month_data = data[data['date'] >= f'{args.month}-01']
+    res = ''
+    for game_type in GAME_TYPES:
+        res += game_type_to_bbcode(game_type.upper(), month_data[month_data['game_type'] == game_type])
+    res += game_type_to_bbcode('DIVERS', month_data[~month_data['game_type'].isin(GAME_TYPES)])
+    output_file = f'Diarium_Strategorum_{args.month}.txt'
+    with open(output_file, 'w') as f:
+        f.write(res)
+    print(f'{output_file} written')
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    args = get_args()
+    main(args)
