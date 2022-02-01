740eb6f4c1ba4dd92697d6b62859890629267706
NicolasGrosjean <grosjen@hotmail.fr>
2022-02-01 19:24:26 +0100

    Add new games

--- mundus/constants.py
+++ mundus/constants.py
@@ -1,13 +1,14 @@
 GAMES = ['Age of Empires III : Definitive Edition', 'Age of Empires II', 'Age of Empires IV', 'Ancient Cities', 'Ancient Worlds : Egypt',
-         'Builder Simulator', 'Builders of China', 'Builders of Egypt',
+         'Builder Simulator', 'Builders of China', 'Builders of Egypt', 'Builders of Greece',
          'Chernobyl Liquidators Simulator', 'Chinese City Constructor ', 'Crusader Kings 3',
          'Crusader Kings III', 'Decisive Campaigns : Ardennes Offensive', 'Diarium Strategorum', 'Distant Kingdoms',
          'El Dorado: The Golden City Builder', 'Empire : Total War', 'Europa Universalis IV', 'Espresso Tycoon',
          'Field of Glory II : Medieval', 'Fields of History: The Great War', 'Going Medieval',
-         'Hearts of Iron', 'HOI4', 'Hegemony III', 'Hellish Quart', 'Hellish quart', 'Humankind', 'Imperator : Rome', 'Knights of Honor 2',
+         'Hearts of Iron', 'HOI4', 'Hegemony III', 'Hellish Quart', 'Hellish quart', 'Humankind', 'Imperator : Rome', 'Kainga', 'Knights of Honor 2',
          'La Porte des Fauconniers', 'Medieval II - Total War', 'Medieval II : Total War', 'Monuments Flipper', 'Mount & Blade II',
-         'Nebuchadnezzar', 'Patron', 'Pharaoh : A New Era', 'Pizza Simulator', 'Project Wunderwaffe', 'Rome : Total War', 'Stadium Renovator', 'Stellaris', 'Sumerians',
-         'Supermarket Manager', 'The Troop', 'Three Kingdoms', 'Timberborn', 'Total War : Troy', 'Total War : Warhammer',
+         'Nebuchadnezzar', 'Patron', 'Pharaoh : A New Era', 'Pizza Simulator', 'Project Wunderwaffe', 'Robin Hood - Sherwood Builders',
+         'Rome : Total War', 'Stadium Renovator', 'Stellaris', 'Sumerians',
+         'Supermarket Manager', 'Terra Nil', 'The Troop', 'Three Kingdoms', 'Timberborn', 'Total War : Troy', 'Total War : Warhammer',
          'Total War : WARHAMMER', 'Troy : Total War', 'Victoria 3', 'Victoria III', 'Viking City Builder', 'Wargame Red Dragon']
 
 GAME_MAPPING = {'Crusader Kings III': 'Crusader Kings 3', 'Hearts of Iron': 'Hearts of Iron IV', 'Hellish Quart': 'Hellish quart', 'HOI4': 'Hearts of Iron IV',
@@ -16,13 +17,16 @@ GAME_MAPPING = {'Crusader Kings III': 'Crusader Kings 3', 'Hearts of Iron': 'Hea
                 'Medieval II - Total War': 'Medieval II : Total War', 'Total War : Troy': 'Troy : Total War', 'Victoria III': 'Victoria 3'}
 
 GESTION = ['Ancient Cities', 'Ancient Worlds : Egypt', 'Builder Simulator',
-                'Builders of China', 'Builders of Egypt', 'Chernobyl Liquidators Simulator', 'Chinese City Constructor ', 'Distant Kingdoms',
-                'El Dorado: The Golden City Builder', 'Espresso Tycoon', 'Going Medieval',
+                'Builders of China', 'Builders of Egypt', 'Builders of Greece', 'Chernobyl Liquidators Simulator', 'Chinese City Constructor ', 'Distant Kingdoms',
+                'El Dorado: The Golden City Builder', 'Espresso Tycoon', 'Going Medieval', 'Kainga',
                 'Monuments Flipper', 'Nebuchadnezzar', 'Patron', 'Pharaoh : A New Era', 'Pizza Simulator', 'Project Wunderwaffe',
-                'Stadium Renovator', 'Sumerians', 'Supermarket Manager', 'Timberborn', 'Viking City Builder']
+                'Robin Hood - Sherwood Builders',
+                'Stadium Renovator', 'Sumerians', 'Supermarket Manager', 'Terra Nil', 'Timberborn', 'Viking City Builder']
 
-GRANDE_STATEGIE = ['Crusader Kings 3', 'Europa Universalis IV', 'Fields of History: The Great War', 'Hearts of Iron IV',
-                'Imperator : Rome', 'Stellaris', 'Victoria 3']
+PARADOX_INTERACTIVE = ['Crusader Kings 3', 'Europa Universalis IV', 'Hearts of Iron IV', 'Imperator : Rome', 'Stellaris', 'Victoria 3']
+
+GRANDE_STATEGIE = PARADOX_INTERACTIVE.copy()
+GRANDE_STATEGIE.append('Fields of History: The Great War')
 
 RPG = ['Hellish quart', 'La Porte des Fauconniers', 'Mount & Blade II : Bannerlord']
 
