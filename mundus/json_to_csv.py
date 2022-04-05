046ad49f335566afc94f8e06c70776031990e6cd
NicolasGrosjean <grosjen@hotmail.fr>
2022-04-05 20:09:15 +0200

    Fix manage yesterday and to day date
    Preprocess yesterday and today expressions in dates

--- mundus/json_to_csv.py
+++ mundus/json_to_csv.py
@@ -1,3 +1,4 @@
+import datetime
 import pandas as pd
 import os
 import argparse
@@ -49,6 +50,11 @@ def main(args):
     json_path = os.path.join(get_data_dir(), relative_json_path)
     print(f'Read the file {json_path}')
     data = pd.read_json(json_path, encoding='utf-8', keep_default_dates=False)
+    today_datetime = datetime.datetime.now()
+    yesterday_str = (today_datetime + datetime.timedelta(days=-1)).strftime('%d-%m-%Y')
+    today_str = today_datetime.strftime('%d-%m-%Y')
+    data['date'] = data['date'].apply(lambda date: date.replace('Hier', yesterday_str))
+    data['date'] = data['date'].apply(lambda date: date.replace('Aujourd\'hui', today_str))
     data['date'] = pd.to_datetime(data['date'], dayfirst=True)
     if os.path.exists(os.path.join(get_data_dir(), 'url_to_game.csv')):
         df = pd.read_csv(os.path.join(get_data_dir(), 'url_to_game.csv'), encoding='ISO-8859-1')
